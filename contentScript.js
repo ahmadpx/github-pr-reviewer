e89a67e1085c0fb9015296771989237af9b44968
Ahmed Abdallah <ahmadpx@hotmail.com>
2020-03-30 16:17:56 +0400

    JSDoc

--- contentScript.js
+++ contentScript.js
@@ -3,6 +3,12 @@ onMessage(function({ filesPatterns, markAll, asViewed }, sender, sendResponse) {
   sendResponse({ done: true });
 });
 
+/**
+ * mark files as reviewed or un-reviewed
+ * @param {Array<{active: Boolean, value: String}>} filesPatterns
+ * @param {Boolean} markAll
+ * @param {Boolean} asViewed
+ */
 function reviewPRFiles({ filesPatterns, markAll, asViewed }) {
   if(markAll) {
     document.querySelectorAll(`.js-reviewed-checkbox`).forEach(input => check(input, asViewed))
@@ -15,10 +21,19 @@ function reviewPRFiles({ filesPatterns, markAll, asViewed }) {
   }
 }
 
+/**
+ * check / un-check files
+ * @param {HTMLInputElement} input
+ * @param {Boolean} asViewed
+ */
 function check(input, asViewed) {
-  return asViewed ? !input.checked && input.click() : input.checked && input.click()
+  asViewed ? !input.checked && input.click() : input.checked && input.click()
 }
 
+/**
+ * listening on messages
+ * @param cb
+ */
 function onMessage(cb) {
   chrome.runtime.onMessage.addListener(cb);
 }
